edf05ac0ba566b2c3f931191553ada97a415c806
RasoulAM <rasoul.am1376@gmail.com>
2018-01-28 07:40:25 +0330

    minor changes

--- project/src/Grammar.java
+++ project/src/Grammar.java
@@ -190,12 +190,9 @@ public class Grammar {
                 change = change || normalizeFollow(r);
             }
         }
-
 //        for (int i = 0; i < nonTerminals.size(); i++) {
 //            System.out.println(nonTerminals.get(i).toString() + " " + follow.get(nonTerminals.get(i)).toString());
 //        }
-
-
     }
 
     private boolean normalizeFollow(Rule rule) {
